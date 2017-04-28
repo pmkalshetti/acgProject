5507452c77aaad6fb1956097544d836973de6af6
Naman Rastogi <naman.rastogi.52@gmail.com>
2017-04-28 19:56:07 +0530

    Delete compile.sh

--- compile.sh
+++
@@ -1,1 +0,0 @@
-g++ -std=c++11 main.cpp Application.cpp -lglut -lGL -lGLU -I /usr/local/include/bullet/ /usr/local/lib/libBulletDynamics.a /usr/local/lib/libBulletCollision.a /usr/local/lib/libLinearMath.a
