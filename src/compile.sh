da4c23dff9dd0de66e0fd43acac8576bd0d5dcf1
Naman Rastogi <naman.rastogi.52@gmail.com>
2017-04-28 16:59:00 +0530

    Updated for getCentroidPosition.cpp

--- src/compile.sh
+++ src/compile.sh
@@ -1,1 +1,3 @@
-g++ -std=c++11 main.cpp Application.cpp -lglut -lGL -lGLU -I ~/include/bullet/ ~/lib/libBulletDynamics.a ~/lib/libBulletCollision.a ~/lib/libLinearMath.a
+g++ -std=c++11 main.cpp Application.cpp -lglut -lGL -lGLU -I /usr/local/include/bullet/ /usr/local/lib/libBulletDynamics.a /usr/local/lib/libBulletCollision.a /usr/local/lib/libLinearMath.a
+
+g++ getCentroidPosition.cpp -lopencv_core -lopencv_features2d -lopencv_highgui -lopencv_imgcodecs -lopencv_video -o getCentroidPosition -L/net/voxel07/misc/me/pratikm/lib/
